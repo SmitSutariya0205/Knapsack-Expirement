b7f03b6b63a252918b220969433e470a6440416d
daksh <ssutariya8801@gmail.com>
2026-03-14 01:18:27 +0530

    build: compile dist/server.js with manual CORS headers

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -5,23 +5,38 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv/config");
 const express_1 = __importDefault(require("express"));
-const cors_1 = __importDefault(require("cors"));
 const db_1 = require("./db");
 const participantRoutes_1 = require("./routes/participantRoutes");
 const authRoutes_1 = require("./routes/authRoutes");
 const app = (0, express_1.default)();
 const port = Number(process.env.PORT || 8787);
-app.use((0, cors_1.default)({
-    origin: [
+// MANUAL CORS INJECTION FOR RENDER
+// Using standard explicit manual headers because the 'cors' package gets blocked during preflight
+app.use((req, res, next) => {
+    const allowedOrigins = [
         'https://arjav5090.github.io',
         'https://smitsutariya0205.github.io',
         'http://localhost:3000',
         'http://localhost:3001',
         ...(process.env.CORS_ORIGIN?.split(',') ?? [])
-    ],
-    credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization', 'x-admin-key']
-}));
+    ];
+    const origin = req.headers.origin;
+    if (origin && allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    else if (allowedOrigins.length > 0) {
+        res.setHeader('Access-Control-Allow-Origin', allowedOrigins[0]);
+    }
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-admin-key');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200);
+    }
+    else {
+        next();
+    }
+});
 app.use(express_1.default.json({ limit: '2mb' }));
 app.use(participantRoutes_1.router);
 app.use(authRoutes_1.router);
